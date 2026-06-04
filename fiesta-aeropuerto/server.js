6d3e28bb163fdc8edd08723633469a93a0171746
rickagpe-ctrl <rickagpe@gmail.com>
2026-06-04 09:23:07 -0600

    Update server.js

--- fiesta-aeropuerto/server.js
+++ fiesta-aeropuerto/server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 const guests = [
-  { id: "g01", name: "Jose Ricardo Almaguer Hernandez",       emoji: "🎉", color: "#6C63FF", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMkuuXLWaMEV1LQhSegsGuEgBYzIpi4JxPYiJYHsBNWy0wAyGyppbnzxqUcvwGt1iBc54WTo&s=10" },
+  { id: "g01", name: "Martha Mendez Palomares",       emoji: "🎉", color: "#6C63FF", photo: "https://cdn.phototourl.com/member/2026-06-04-c07af0d5-75b4-45f4-9167-f945f4e30635.jpg" },
   { id: "g02", name: "Carlos López",     emoji: "✈️",  color: "#FF6584", photo: "" },
   { id: "g03", name: "María Rodríguez",  emoji: "🌟",  color: "#43B89C", photo: "" },
   { id: "g04", name: "José Martínez",    emoji: "🎊",  color: "#F5A623", photo: "" },
